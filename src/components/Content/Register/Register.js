2fd507411a3b3e9f332cc15d5656df3091458f1c
maxvanwijnen <maxvanwijnen@gmail.com>
2022-11-30 11:06:35 +0100

    form validator register

--- src/components/Content/Register/Register.js
+++ src/components/Content/Register/Register.js
@@ -7,26 +7,96 @@ export const Register = () => {
     const { funcRegister } = useContext(AuthContext);
 
 
+    const [fields, setFields] = useState({
+        email:"",
+        password:"",
+        password2:""
+    });
+
+    const [error, setError] = useState({});
+
+
+    const updateField = (e) => {
+
+        const {name, value} = e.target;
+
+        setFields({
+            ...fields,
+            [name]:value
+        })
+    }
+
+    const passwordsMatch = () => {
+        return fields.password === fields.password2;
+    }
+
+    const validEmail = () => {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(fields.email);
+    }
+
+    const formValidator = () => {
+
+        let errorList = {};
+
+        if (passwordsMatch()){
+            errorList = {...errorList,passwordsDontMatch:false};
+        }
+        else {
+            errorList = {...errorList,passwordsDontMatch:true};
+        }
+
+        if (validEmail()){
+            errorList = {...errorList,invalidEmail:false};
+        }
+        else {
+            errorList = {...errorList,invalidEmail:true};
+        }
+
+        setError({...error,...errorList});
+
+        return !errorList.invalidEmail && !errorList.passwordsDontMatch;
+
+    }
 
     const submit = (e) => {
         e.preventDefault();
+        if (formValidator()){
 
-        funcRegister(e.target[0].value,e.target[1].value);
+        }
 
 
     }
 
+    console.log(error);
     return (
         <form className={css['login-form']} onSubmit={(e)=>submit(e)}>
+            {error.passwordsDontMatch && <div>Password's don't match</div>}
+            {error.invalidEmail && <div>Please enter a valid emailadsress</div>}
+
             <div>Register</div>
+
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" />
+            <input type="text"
+                   name="email"
+                   value={fields.email}
+                   onChange={(e)=>updateField(e)}
+            />
 
             <label htmlFor="password">Password</label>
-            <input type="password" name="password"/>
+            <input
+                type="password"
+                name="password"
+                value={fields.password}
+                onChange={(e)=>updateField(e)}
+            />
 
             <label htmlFor="password2">Type password again</label>
-            <input type="password" name="password2"/>
+            <input
+                type="password"
+                name="password2"
+                value={fields.password2}
+                onChange={(e)=>updateField(e)}
+            />
 
             <button type="submit">Register</button>
         </form>
